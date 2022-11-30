b099e9d0429c77a263978969d08aa8d302f57aea
chinmayn1 <mathakarichinu@gmail.com>
2022-11-29 23:22:36 -0500

    Deleted IWS PR r 5

--- IWS_Weekly_Project_Report_5/backend/.flaskenv
+++
@@ -1,4 +0,0 @@
-export FLASK_ENV=development
-export FLASK_APP=src
-export SQLALCHEMY_DATABASE_URI=mysql+pymysql://root:Jaymahakali10@localhost/eassessment
-export JWT_SECRET_KEY='JWT_SECRET_KEY'
--- IWS_Weekly_Project_Report_5/backend/src/services/mail/__init__.py
+++
@@ -1,8 +0,0 @@
-from datetime import datetime, timedelta
-from sendgrid import SendGridAPIClient
-from sendgrid.helpers.mail import Mail
-from flask_jwt_extended import create_access_token
-import os
-from flask_mail import Mail, Message
-
-mail=Mail()
